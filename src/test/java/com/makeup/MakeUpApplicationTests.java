9bbf5676de858375a1bbe60d5581beb7680a1b94
이지광 <ijigwang@ijigwang-ui-MacBookPro.local>
2024-02-05 15:29:02 +0900

    init: 개발환경 설정

---
+++ src/test/java/com/makeup/MakeUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
