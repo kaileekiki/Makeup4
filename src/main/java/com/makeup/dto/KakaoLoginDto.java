b0dfc84c98b5944df473a544f79d8f84ffaef5ce
kaileekiki <lucas0606@handong.ac.kr>
2024-02-06 14:43:18 +0900

    카카오 추가

---
+++ src/main/java/com/makeup/dto/KakaoLoginDto.java
@@ -0,0 +1,13 @@
+package com.makeup.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Builder
+@Getter
+public class KakaoLoginDto {
+
+    public String accessToken;
+    public String refreshToken;
+}
